899999ae7cdb376a95b89c9fa8b775dd695b56cc
Vivek Kumar Singh <vivekhappy786@gmail.com>
2022-01-22 19:37:37 +0530

    Add Linked list

--- Linked List/1_linkedList.cpp
+++ Linked List/1_linkedList.cpp
@@ -23,6 +23,30 @@ void print(Node *head)
     }
 }
 
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
 int main()
 {
     // Statically
