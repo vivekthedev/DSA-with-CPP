ef0e55408d782e4fd2f379c375d55cdd9144ffc8
Vivek <contactvivekhere@gmail.com>
2022-08-21 09:25:45 +0530

    stack using template

---
+++ Stacks/02_stacks_using_templates.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+
+class Stack
+{
+    T *data;
+    int capacity;
+    int nextIndex;
+
+public:
+    Stack()
+    {
+        data = new T[4];
+        nextIndex = 0;
+        capacity = 4;
+    }
+    int size()
+    {
+        return nextIndex;
+    }
+    bool isEmpty()
+    {
+        return nextIndex == 0;
+    }
+    void push(T element)
+    {
+        if (nextIndex == capacity)
+        {
+            T *newData = new T[2 * capacity];
+            for (int i; i < capacity; i++)
+            {
+                newData[i] = data[i];
+            }
+            capacity *= 2;
+            delete[] data;
+            data = newData;
+        }
+        data[nextIndex++] = element;
+    }
+    T pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty\n";
+            // return NULL;
+        }
+        else
+            return data[--nextIndex];
+    }
+    T top()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty\n";
+            // return NULL;
+        }
+        else
+            return data[nextIndex - 1];
+    }
+};
+
+int main()
+{
+    Stack<int> s1;
+    Stack<char> s2;
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s2.push('V');
+    s2.push('i');
+    s2.push('v');
+    s2.push('e');
+    s2.push('k');
+    cout << s1.pop() << endl;
+    cout << s2.top();
+}
