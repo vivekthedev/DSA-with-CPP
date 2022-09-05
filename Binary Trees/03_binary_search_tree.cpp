4f5f09591a61b3c53d909c197c4cda4385ef62e7
Vivek <contactvivekhere@gmail.com>
2022-09-05 12:31:18 +0530

    binary search trees

---
+++ Binary Trees/03_binary_search_tree.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <queue>
+#include "BinaryTree.h"
+using namespace std;
+
+void printTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    if (root->left)
+    {
+        cout << "L " << root->left->data;
+    }
+    if (root->right)
+    {
+        cout << "R " << root->right->data;
+    }
+    cout << endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+BinaryTreeNode<int> *takeInputLevel()
+{
+    int rootData;
+    cout << "enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "enter left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        cout << "enter right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+bool binary_search(BinaryTreeNode<int> *root, int data)
+{
+    bool ans = false;
+    if (root == NULL)
+    {
+        return false;
+    }
+    else if (root->data > data)
+    {
+        ans = binary_search(root->right, data);
+    }
+    else if (root->data < data)
+    {
+        ans = binary_search(root->right, data);
+    }
+    else if (root->data == data)
+    {
+        ans = true;
+    }
+    return ans;
+}
+int main()
+{
+    BinaryTreeNode<int> *root = takeInputLevel();
+    cout << binary_search(root, 7);
+}
