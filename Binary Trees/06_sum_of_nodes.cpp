4bce41831ae3a04e5802a88deff6321665d8ac97
Vivek <contactvivekhere@gmail.com>
2022-09-07 21:15:27 +0530

    sum of nodes

---
+++ Binary Trees/06_sum_of_nodes.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+#include "BinaryTree.h"
+using namespace std;
+
+void printTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    if (root->left)
+    {
+        cout << "L " << root->left->data;
+    }
+    if (root->right)
+    {
+        cout << "R " << root->right->data;
+    }
+    cout << endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+BinaryTreeNode<int> *takeInputLevel()
+{
+    int rootData;
+    cout << "enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "enter left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        cout << "enter right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+int sum_of_nodes(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = sum_of_nodes(root->left);
+    int r = sum_of_nodes(root->right);
+
+    return root->data + l + r;
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = takeInputLevel();
+    cout << sum_of_nodes(root) << " ";
+}
