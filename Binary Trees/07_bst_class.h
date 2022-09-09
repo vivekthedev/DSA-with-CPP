2e9fe9dc75ac259d06957ae836d42532dfe9274a
Vivek <contactvivekhere@gmail.com>
2022-09-09 22:37:37 +0530

    bst class

---
+++ Binary Trees/07_bst_class.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "BinaryTree.h"
+using namespace std;
+
+class BST
+{
+    BinaryTreeNode<int> *root;
+    bool hasData(int data, BinaryTreeNode<int> *root)
+    {
+        if (root == NULL)
+        {
+            return false;
+        }
+        if (root->data == data)
+        {
+            return true;
+        }
+        else if (data < root->data)
+        {
+            return hasData(data, root->left);
+        }
+        else
+        {
+            return hasData(data, root->right);
+        }
+    }
+
+public:
+    bool hasData(int data)
+    {
+        return hasData(data, root);
+    }
+    void insert(int data)
+    {
+        }
+    void deleteData(int data)
+    {
+    }
+    BST()
+    {
+        root = NULL;
+    }
+    ~BST()
+    {
+        delete root;
+    }
+};
